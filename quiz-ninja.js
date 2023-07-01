b26de5d01e5f25731dc35d4bf08c1ba5631e5bd7
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2023-07-01 14:51:22 -0400

    load manual grading feedback

--- quiz-ninja.js
+++ quiz-ninja.js
@@ -97,6 +97,11 @@
             }
         },
         {
+            id: "Load feedback",
+            title: "Load feedback for manual grading",
+            handler: loadManualFeedback
+        },
+        {
             id: "Cleanup Manual Grading",
             title: "Cleanup Manual Grading",
             handler: () => {
@@ -118,6 +123,43 @@
             }
         }
     ];
+
+    function loadManualFeedback() {
+        const allComments = JSON.parse(prompt("paste comments JSON here: "));
+
+        const allStudents = new Set(allComments.map(x => x.Name))
+        console.log(allComments, allStudents);
+
+        function getGrade(name) {
+            for (let row of allComments) {
+                if (name == row.name) {
+                    allStudents.delete(name);
+                    return row;
+                }
+            }
+            console.warn("Missing student:", name)
+            return {"Score": "", "Comments": ""};
+        }
+
+        window.$('input[name$="-mark"]').each(function(idx) {
+            let elt = window.$(this);
+            let container = elt.parents('.que');
+            let parent = container.prevAll('h4').first().text();
+            let name = /Attempt number \d+ for (.+)$/.exec(parent)[1];
+            let grade = getGrade(name);
+            elt.val(grade.score);
+            let editor = container.find('.editor_atto_content');
+            if (editor.length != 1) {
+                console.warn("Failed to find editor " + name);
+            }
+            editor.html(grade.comments)
+        })
+
+        console.log(allStudents)
+
+        setTimeout(function() { window.$('.icon.fa-code').click(); }, 1*1000);
+        setTimeout(function() { window.$('.icon.fa-code').click(); }, 5*1000);
+    }
 
     let reusePrevAction = {
         id: "ReusePrev",
