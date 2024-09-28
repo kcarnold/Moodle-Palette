78afcbb44b0a96ea2804dadb034a8097ccb18c5b
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2024-09-28 14:02:10 -0400

    Description

--- moodle-ninja.js
+++ moodle-ninja.js
@@ -2,7 +2,7 @@
 // @name         Moodle Ninja
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  Control Moodle with the keyboard.
 // @author       You
 // @match        https://moodle.calvin.edu/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=calvin.edu
