8e51d69a499a2f382465b1dac4bf029e7f1f254a
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2026-02-27 17:00:44 -0500

    Cleanup

--- my-extension/src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Utility function for sending messages between different parts of the extension.
- * @param message - The message to send.
- * @param callback - The callback function to handle the response.
- */
-export function sendMessage(message: any, callback: (response: any) => void): void {
-  chrome.runtime.sendMessage(message, callback);
-}
