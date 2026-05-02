1a0ab4252113e8336b221545a4ab802bfabe9e26
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2026-05-02 17:02:13 -0400

    lint

--- extension/content/discussion-full-credit.js
+++ extension/content/discussion-full-credit.js
@@ -9,17 +9,17 @@
 document.addEventListener('keydown', (event) => {
     if (event.repeat || event.defaultPrevented) return;
     if (event.target.tagName === "TEXTAREA") return;
-    if (event.key == "f") {
+    if (event.key === "f") {
         document.querySelectorAll('.criterion [role="radiogroup"]').forEach(criterion => {
             const levels = [...criterion.querySelectorAll('[type="radio"]')];
             levels[levels.length - 1].click();
         });
-    } else if (event.key == "m") {
+    } else if (event.key === "m") {
         document.querySelectorAll('.criterion [role="radiogroup"]').forEach(criterion => {
             const levels = [...criterion.querySelectorAll('[type="radio"]')];
             levels[1].click();
         });
-    } else if (event.key == '>') {
+    } else if (event.key === '>') {
         const btn = document.querySelector('.page-link[title*="proceed to the next user"]');
         if (btn) btn.click();
     }
